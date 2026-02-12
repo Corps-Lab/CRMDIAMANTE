488d53e55e1fdaa082ca0a2f3071903f894b0eb8
FITGA DEVELOPER <gugufadao@gmail.com>
2026-02-12 13:35:31 -0300

    chore: use relative imports to avoid alias resolution issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
-import { ClientProvider } from "@/contexts/ClientContext";
-import { ContractProvider } from "@/contexts/ContractContext";
-import { TransactionProvider } from "@/contexts/TransactionContext";
-import { DemandProvider } from "@/contexts/DemandContext";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ClientProvider } from "./contexts/ClientContext";
+import { ContractProvider } from "./contexts/ContractContext";
+import { TransactionProvider } from "./contexts/TransactionContext";
+import { DemandProvider } from "./contexts/DemandContext";
 
 import Index from "./pages/Index";
 import Clientes from "./pages/Clientes";
